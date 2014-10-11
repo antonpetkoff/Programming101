1e6a8e48a61d225553e75e3dc4e5f22035eb86a5
tonynho <tonynho010@gmail.com>
2014-10-12 00:58:54 +0300

    fix magic square solution

--- Programming101/week0/magic_square.py
+++ Programming101/week0/magic_square.py
@@ -1,31 +1,28 @@
 
 def magic_square(matrix):
+    n = len(matrix)
     leftDiagonal = 0
     rightDiagonal = 0
-    currentColSum = 0
-    currentRowSum = 0
-    previousColSum = None
-    previousRowSum = None
+    rowSums = [0 for i in range(n)]
+    colSums = [0 for i in range(n)]
 
-    for row in range(len(matrix[0])):
-        for col in range(len(matrix)):
+    for row in range(n):
+        for col in range(n):
             if row == col:
                 leftDiagonal += matrix[row][col]
             if row + col == len(matrix) - 1:
                 rightDiagonal += matrix[row][col]
-            currentColSum += matrix[row][col]
-        currentRowSum = sum(matrix[row])
-
-        if currentRowSum != currentColSum:
-            return False
-        if previousColSum is not None and currentColSum != previousColSum:
-            return False
-        if previousRowSum is not None and currentRowSum != previousRowSum:
-            return False
-        currentColSum = 0
+            colSums[col] += matrix[row][col]
+        rowSums[row] = sum(matrix[row])
 
     if leftDiagonal != rightDiagonal:
         return False
+
+    sums = rowSums + colSums
+    for i in range(1, len(sums)):
+        if sums[i-1] != sums[i]:
+            return False
+
     return True
 
 

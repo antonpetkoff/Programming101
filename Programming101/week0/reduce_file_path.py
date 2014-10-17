4387a50bc97bc30ef9ba9cdf3e699308e02f9835
tonynho <tonynho010@gmail.com>
2014-10-17 19:47:01 +0300

    tests...

--- Programming101/week0/reduce_file_path.py
+++ Programming101/week0/reduce_file_path.py
@@ -25,19 +25,3 @@ def reduce_file_path(path):
         for elem in fixedPath:
             result += "/" + elem
     return result
-
-
-def main():
-    print(reduce_file_path("/"))
-    print(reduce_file_path("/srv/../"))
-    print(reduce_file_path("/srv/www/htdocs/wtf/"))
-    print(reduce_file_path("/srv/www/htdocs/wtf"))
-    print(reduce_file_path("/srv/./././././"))
-    print(reduce_file_path("/etc//wtf/"))
-    print(reduce_file_path("/etc/../etc/../etc/../"))
-    print(reduce_file_path("//////////////"))
-    print(reduce_file_path("/../"))
-
-
-if __name__ == '__main__':
-    main()

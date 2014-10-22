9015ba6c31262b0951f06e264c312ad9305b843d
tonynho <tonynho010@gmail.com>
2014-10-22 18:23:15 +0300

    finish Fight

--- Programming101/week2/Dungeons_and_Pythons/fight.py
+++ Programming101/week2/Dungeons_and_Pythons/fight.py
@@ -7,24 +7,22 @@ class Fight():
         self.hero = hero
         self.orc = orc
 
+    def handle_hit(self, attacker, opponent):
+        if attacker.weapon.critical_hit():
+            damage = attacker.attack() * 2
+        else:
+            damage = attacker.attack()
+        expr = "{} ({} Health) attacks {} ({} Health) with {} damage"
+        print(expr.format(attacker.name, attacker.health,
+                          opponent.name, opponent.health, damage))
+        opponent.take_damage(damage)
+
     def simulate_fight(self):
         coin_flip = random()
         round = 0 if coin_flip < 0.5 else 1
         while(self.hero.is_alive() and self.orc.is_alive()):
             if round % 2 == 0:  # hero attacks
-                if self.hero.weapon.critical_hit():
-                    damage = self.hero.attack() * 2
-                else:
-                    damage = self.hero.attack()
-                expr = "Hero @ {} health attacks Orc @ {} health with {} damage"
-                print(expr.format(self.hero.health, self.orc.health, damage))
-                self.orc.take_damage(damage)
+                self.handle_hit(self.hero, self.orc)
             else:               # orc attacks
-                if self.orc.weapon.critical_hit():
-                    damage = self.orc.attack() * 2
-                else:
-                    damage = self.orc.attack()
-                expr = "Orc @ {} health attacks Hero @ {} health with {} damage"
-                print(expr.format(self.orc.health, self.hero.health, damage))
-                self.hero.take_damage(damage)
+                self.handle_hit(self.orc, self.hero)
             round += 1

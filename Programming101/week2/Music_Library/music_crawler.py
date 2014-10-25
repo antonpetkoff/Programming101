a58c77b50188d4e7cc2b9bfa43d200ea1229246c
tonynho <tonynho010@gmail.com>
2014-10-25 11:38:59 +0300

    finish MusicCrawler

--- Programming101/week2/Music_Library/music_crawler.py
+++ Programming101/week2/Music_Library/music_crawler.py
@@ -1,5 +1,6 @@
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3
+from fnmatch import fnmatch
 from song import Song
 from playlist import Playlist
 import os
@@ -12,16 +13,17 @@ class MusicCrawler:
 
     def generate_playlist(self):
         playlist = Playlist("Playlist")
-        for mp3_file in os.listdir(self.directory):
-            id3 = ID3(self.directory + mp3_file)
-            mp3 = MP3(self.directory + mp3_file)
-            song = Song(id3['TIT2'],
-                        id3['TPE1'],
-                        id3['TALB'],
-                        5,
-                        mp3.info.length,
-                        mp3.info.bitrate)
-            playlist.add_song(song)
+        for file_name in os.listdir(self.directory):
+            if fnmatch(file_name, "*.mp3"):
+                id3 = ID3(self.directory + file_name)
+                mp3 = MP3(self.directory + file_name)
+                song = Song(id3['TIT2'],
+                            id3['TPE1'],
+                            id3['TALB'],
+                            5,
+                            mp3.info.length,
+                            mp3.info.bitrate)
+                playlist.add_song(song)
         return playlist
 
 

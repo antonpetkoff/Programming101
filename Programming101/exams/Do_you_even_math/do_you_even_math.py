a56202d64122656b72027598c50a5b05947db2e4
tonynho <tonynho010@gmail.com>
2014-12-07 00:44:24 +0200

    db work

--- Programming101/exams/Do_you_even_math/do_you_even_math.py
+++ Programming101/exams/Do_you_even_math/do_you_even_math.py
@@ -13,14 +13,29 @@ class DoYouEvenMath:
         Base.metadata.create_all(engine)
         self.session = Session(bind=engine)
 
-    def player_exists(self):
-        pass
+    def player_exists(self, name):
+        select = self.session.query(Player).filter(Player.name == name).all()
+        return len(select) == 1
 
-    def add_player(self):
-        pass
+    def add_player(self, name):
+        if self.player_exists(name):
+            return
+
+        self.session.add(Player(name=name, score=0))
+        self.session.commit()
 
     def update_score(self):
         pass
 
     def get_highscores(self):
         pass
+
+
+def main():
+    db = DoYouEvenMath()
+    db.add_player("Dingo")
+    print(db.player_exists("Dingo"))
+    print(db.player_exists("asd"))
+
+if __name__ == '__main__':
+    main()
